9917489a070928b9a8653256a6e0717f043b876e
olegivo <olegivo@gmail.com>
2020-10-18 16:39:15 +0300

    Stage 1/6: String theory

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -1,5 +1,16 @@
 package search
 
+import java.util.*
+
 fun main() {
-    println("Hello, World!")
+    val source = scanner.nextLine().split(' ')
+    val find = scanner.nextLine()
+    println(find(source, find) ?: "Not found")
 }
+
+private fun find(source: List<String>, find: String?): Int? =
+        source.indices
+                .firstOrNull { source[it] == find }
+                ?.let { it + 1 }
+
+val scanner = Scanner(System.`in`)
