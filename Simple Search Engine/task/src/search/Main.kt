721b65578b84ad032728a587dcd3c6326aceadfd
olegivo <olegivo@gmail.com>
2020-10-24 15:33:26 +0300

    Stage 3/6: User menu + Stage 4/6: X-files

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -2,10 +2,33 @@ package search
 
 import java.util.*
 
-fun main() {
+fun main(args: Array<String>) {
+    val data = "--data"
+    val commands = arrayOf(data)
+    val paramByCommand = args.mapIndexed { index, s -> index to s }
+            .filter { commands.contains(it.second) }
+            .associate { it.second to args[it.first + 1] }
+
     val searchEngine = SearchEngine()
-    searchEngine.inputPeoples()
-    searchEngine.search()
+    paramByCommand[data]?.let {
+        searchEngine.readFile(it)
+    } ?: run{
+        searchEngine.inputPeoples()
+    }
+
+    while (scanner.hasNextLine()) {
+        println("""
+=== Menu ===
+1. Search information.
+2. Print all data.
+0. Exit.""")
+        when (scanner.nextLine().toInt()) {
+            1 -> searchEngine.findPerson()
+            2 -> searchEngine.listOfPeople()
+            0 -> break
+            else -> println("Incorrect option! Try again.")
+        }
+    }
 }
 
 val scanner = Scanner(System.`in`)
--- Simple Search Engine/task/src/search/SearchEngine.kt
+++ Simple Search Engine/task/src/search/SearchEngine.kt
@@ -1,27 +1,33 @@
 package search
 
+import java.io.File
+
 class SearchEngine {
-    private lateinit var lines: List<String>
+    private lateinit var persons: List<String>
 
     fun inputPeoples() {
         println("Enter the number of people:")
         val n = scanner.nextLine().toInt()
         println("Enter all people:")
-        lines = (1..n).map { scanner.nextLine() }
+        persons = (1..n).map { scanner.nextLine() }
     }
 
-    fun search() {
+    fun readFile(fileName: String) {
+        persons = File(fileName).readLines()
+    }
+
+    fun findPersons() {
         println("Enter the number of search queries:")
         val q = scanner.nextLine().toInt()
         repeat(q) {
-            searchItem()
+            findPerson()
         }
     }
 
-    private fun searchItem() {
-        println("Enter data to search people:")
+    fun findPerson() {
+        println("Enter a name or email to search all suitable people.")
         val query = scanner.nextLine()
-        val searchResults = lines.filter { it.contains(query, ignoreCase = true) }
+        val searchResults = persons.filter { it.contains(query, ignoreCase = true) }
         if (searchResults.isEmpty()) {
             println("No matching people found.")
         } else {
@@ -29,6 +35,13 @@ class SearchEngine {
             searchResults.forEach {
                 println(it)
             }
+        }
+    }
+
+    fun listOfPeople() {
+        println("=== List of people ===")
+        persons.forEach {
+            println(it)
         }
     }
 }
